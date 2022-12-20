600dc24153b6d8f0d6badc25024ac288b0fa5395
Tuomo <tuokri@tuta.io>
2022-12-20 16:58:59 +0200

    Log all imports and exports with log4net

--- Program.cs
+++ Program.cs
@@ -4,75 +4,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UELib;
+using UELib.Core;
+
+using log4net;
+using log4net.Config;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace RS2PackageTool
 {
     internal static class Program
     {
-        private static List<string> _cNames = new List<string> { "StaticMeshActor", "StaticMeshComponent" };
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        private static readonly List<string> CNames =
+            new List<string> { "StaticMeshActor", "StaticMeshComponent" };
 
         private static void Main(string[] args)
         {
             var package = UnrealLoader.LoadPackage(args[0], System.IO.FileAccess.Read);
-            Console.WriteLine($"Version: {package.Summary.Version}\n");
+            log.Info($"Version: {package.Summary.Version}\n");
 
             package.InitializePackage();
             package.InitializeExportObjects();
             package.InitializeImportObjects();
 
             var imports = package.Imports;
-            var someImports = imports.GetRange(0, 25);
-
+            log.InfoFormat("imports.Count: {0}\n", imports.Count);
             imports.ForEach(PrintImportTableItem);
 
-            /*
-            // var someObjects = package.Objects.GetRange(0, 5);
-            foreach (var obj in package.Objects)
-            {
-                try
-                {
-                    obj.BeginDeserializing();
+            var exports = package.Exports;
+            log.InfoFormat("exports.Count: {0}\n", exports.Count);
+            exports.ForEach(PrintExportTableItem);
 
-                    if (obj.Class.Name != "StaticMeshActor")
-                    {
-                        continue;
-                    }
-
-                    Console.WriteLine($"Name: {obj.Name}");
-                    Console.WriteLine($"Class: {obj.Class?.Name}");
-                    Console.WriteLine($"Outer: {obj.Outer}");
-
-                    var output = obj.Decompile();
-                    Console.WriteLine(output);
-
-                    Console.WriteLine("");
-                }
-                catch (Exception ex)
-                {
-                    // Console.WriteLine(ex.GetType());
-                }
-            }
-            // someObjects.ForEach(i => Console.WriteLine("{0.}{1}"));
-            */
+            // Console.ReadLine();
         }
 
         private static void PrintImportTableItem(UImportTableItem importTableItem)
         {
-            if (!_cNames.Contains(importTableItem.ClassName))
-            {
-                return;
-            }
+            // if (!CNames.Contains(importTableItem.ClassName))
+            // {
+            //     return;
+            // }
 
-            Console.WriteLine(importTableItem);
-            Console.WriteLine("PackageName: {0}", importTableItem.PackageName);
-            Console.WriteLine("ClassName: {0}", importTableItem.ClassName);
-            Console.WriteLine("Index: {0}", importTableItem.Index);
-            Console.WriteLine("Offset: {0}", importTableItem.Offset);
-            Console.WriteLine("Size: {0}", importTableItem.Size);
-            Console.WriteLine("Outer: {0}", importTableItem.Outer);
-            Console.WriteLine("Object: {0}", importTableItem.Object);
-            Console.WriteLine("Owner: {0}", importTableItem.Owner);
-            Console.WriteLine("... ... ... ... ... ... ... ...");
+            var obj = importTableItem.Object;
+
+            log.Info("... ... ... ... ... ... ... ...");
+
+            log.InfoFormat("importTableItem : {0}", importTableItem);
+            log.InfoFormat("PackageName     : {0}", importTableItem.PackageName);
+            log.InfoFormat("ClassName       : {0}", importTableItem.ClassName);
+            log.InfoFormat("Index           : {0}", importTableItem.Index);
+            log.InfoFormat("Offset          : {0}", importTableItem.Offset);
+            log.InfoFormat("Size            : {0}", importTableItem.Size);
+            log.InfoFormat("Outer           : {0}", importTableItem.Outer);
+            log.InfoFormat("Owner           : {0}", importTableItem.Owner);
+            log.InfoFormat("ObjectName      : {0}", importTableItem.ObjectName);
+
+            obj.BeginDeserializing();
+            log.InfoFormat("Object          : {0}", obj);
+            log.InfoFormat("Class           : {0}", obj.Class);
+            log.InfoFormat("Properties      : {0}", obj.Properties);
+            log.InfoFormat("GetClassName()  : {0}", obj.GetClassName());
+            log.InfoFormat("Decompile()     : {0}", obj.Decompile());
+        }
+
+        private static void PrintExportTableItem(UExportTableItem exportTableItem)
+        {
+            var obj = exportTableItem.Object;
+
+            log.Info("... ... ... ... ... ... ... ...");
+
+            log.InfoFormat("exportTableItem : {0}", exportTableItem);
+            log.InfoFormat("PackageGuid     : {0}", exportTableItem.PackageGuid);
+            log.InfoFormat("Class           : {0}", exportTableItem.Class);
+            log.InfoFormat("Index           : {0}", exportTableItem.Index);
+            log.InfoFormat("Offset          : {0}", exportTableItem.Offset);
+            log.InfoFormat("Size            : {0}", exportTableItem.Size);
+            log.InfoFormat("Outer           : {0}", exportTableItem.Outer);
+            log.InfoFormat("Owner           : {0}", exportTableItem.Owner);
+            log.InfoFormat("ObjectName      : {0}", exportTableItem.ObjectName);
+
+            obj.BeginDeserializing();
+            log.InfoFormat("Object          : {0}", obj);
+            log.InfoFormat("Class           : {0}", obj.Class);
+            log.InfoFormat("Properties      : {0}", obj.Properties);
+            log.InfoFormat("GetClassName()  : {0}", obj.GetClassName());
+            log.InfoFormat("Decompile()     : {0}", obj.Decompile());
         }
     }
 }
